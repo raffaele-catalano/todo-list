f8768a55bb8fdece7342fa8a4b869195c331a050
raffaele-catalano <raf.catalano@gmail.com>
2023-10-26 23:06:13 +0200

    fixed groups visualization in add_task

--- get_groups.php
+++ get_groups.php
@@ -1,9 +1,14 @@
 <?php
+session_start();
+$userId = $_SESSION['user_id'];
 try {
     include 'connection.php';
 
     // query SQL per ottenere i gruppi dal database
-    $getGroups = $db->query("SELECT id, group_name FROM groups");
+    // $getGroups = $db->query("SELECT id, group_name FROM groups");
+    $getGroups = $db->query("SELECT id, group_name FROM groups WHERE groups.id IN (SELECT group_id FROM users_groups WHERE users_groups.user_id = ?)");
+
+    $getGroups->execute([$userId]);
     $groups = $getGroups->fetchAll(PDO::FETCH_ASSOC);
 
     // invia i gruppi come risposta JSON
